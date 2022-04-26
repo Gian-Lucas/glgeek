6a6170b5c957204b71989806591f73ab4c161134
= <=>
2022-04-26 15:13:42 -0300

    auth with google

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,25 @@
-import { Heading } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { useSession, signIn, signOut } from "next-auth/client";
+import { useEffect } from "react";
 
-export default function Home() {
-  return <Heading>Hello World</Heading>;
+export default function Component() {
+  const { "0": session } = useSession();
+
+  useEffect(() => {
+    console.log(session);
+  }, [session]);
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <Button onClick={() => signOut()}>Sign out</Button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <Button onClick={() => signIn("google")}>Sign in</Button>
+    </>
+  );
 }
