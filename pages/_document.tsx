67efa8f07dfa55899800ad0b9404f565d7fe3195
= <=>
2022-04-26 12:57:41 -0300

    initial config

---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+import { ColorModeScript, theme } from "@chakra-ui/react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
---
+++ styles/theme.ts
@@ -0,0 +1,21 @@
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
+
+export const theme = extendTheme({
+  config,
+  fonts: {
+    heading: "Roboto",
+    body: "Roboto",
+  },
+  styles: {
+    global: {
+      html: {
+        scrollBehavior: "smooth",
+      },
+    },
+  },
+});
