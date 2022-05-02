cf5b7c482a66b0488caf1cb476c7c4032747d6b2
= <=>
2022-05-02 14:00:39 -0300

    default theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,6 @@
 import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
-const config: ThemeConfig = {
-  initialColorMode: "dark",
-};
-
 export const theme = extendTheme({
-  config,
   fonts: {
     heading: "Roboto",
     body: "Roboto",
